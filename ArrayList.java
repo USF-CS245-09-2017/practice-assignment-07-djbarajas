f9a9b8ced1e73a9c478d0f666fe8380ed0ee9045
djbarajas <35737585+djbarajas@users.noreply.github.com>
2019-03-28 10:52:54 -0700

    commented for ArrayList

--- ArrayList.java
+++ ArrayList.java
@@ -3,12 +3,22 @@ import java.util.Arrays;
 public class ArrayList<T> implements List<T> {
     int SIZE ;
     T arr[];
-
+    /*
+    * ArrayList()
+    * constructor, creates an Arraylist of size 10
+    * with SIZE being the amount of elements that are
+    * actually in the list
+     */
     public ArrayList(){
         SIZE = 0;
         arr =  (T[]) new Object[10];
     }
-
+    /*
+    * add()
+    * if the size is equal to the actual length
+    * then grow the array, otherwise ass the item
+    * and increment size
+     */
     @Override
     public void add(T item) {
         if(SIZE == arr.length) {
@@ -17,7 +27,11 @@ public class ArrayList<T> implements List<T> {
 
         arr[SIZE++] = item;
     }
-
+    /*
+    * add()
+    * again grow the array if it reaches a limit
+    * otherwise add the item at a position and increment size
+     */
     @Override
     public void add(int pos, T item) {
         if(SIZE == arr.length) {
@@ -26,12 +40,19 @@ public class ArrayList<T> implements List<T> {
         arr[pos] = item;
         ++SIZE;
     }
-
+    /*
+    * get()
+    * gets the value stored in the position
+     */
     @Override
     public T get(int pos) {
         return arr[pos];
     }
-
+    /*
+    * remove()
+    * removes the element then copies every element
+    * after to the position before it
+     */
     @Override
     public T remove(int pos) {
         T thing = arr[pos];
@@ -42,11 +63,18 @@ public class ArrayList<T> implements List<T> {
 
         return thing;
     }
-
+    /*
+    * size()
+    * returns the amount of elements in the arraylist
+     */
     @Override
     public int size() {
         return SIZE;
     }
+    /*
+    * grow()
+    * grows by copying the array to another double the size
+     */
 
     public void grow(){
         arr = Arrays.copyOfRange(arr,0, arr.length*2);
